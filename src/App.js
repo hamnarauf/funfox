9e05e1fd8a7b9a7f9efffde0acb06bf36d5e990a
Hamna Rauf <hamnarauf7@gmail.com>
2024-03-30 16:08:40 +0500

    Add default index

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import { slidesDeck } from "./constants/slidesDeck";
 import { useState } from "react";
 
 function App() {
-  const [activeSlide, setActiveSlide] = useState(0);
+  const defaultSlideIndex = 0;
+  const [activeSlide, setActiveSlide] = useState(defaultSlideIndex);
 
   return (
     <BrowserRouter>
@@ -12,7 +13,12 @@ function App() {
         <Routes>
           <Route
             path="/"
-            element={<Navigate to={slidesDeck[0].path} replace={true} />}
+            element={
+              <Navigate
+                to={slidesDeck[defaultSlideIndex].path}
+                replace={true}
+              />
+            }
           />
           {slidesDeck.map((slide, index) => (
             <Route key={index} path={slide.path} element={slide.element} />
