1ddccafab7183ee9e5a11991df5a05d869337dee
Hamna Rauf <hamnarauf7@gmail.com>
2024-03-31 00:52:00 +0500

    Fix lesson footer background

--- src/components/LessonFooter.js
+++ src/components/LessonFooter.js
@@ -1,7 +1,7 @@
 export default function LessonFooter({ points }) {
   return (
     <div className="paragraph relative bg-peach rounded-[20px]">
-      <div className="md:px-9 sm:px-2 py-2 my-2 mx-5 lesson-footer">
+      <div className="lg:px-9 md:pr-0 md:pl-7 sm:px-2 py-2 my-2 mx-5 lesson-footer max-w-[1000px]">
         {points.map((point, index) => (
           <div key={index}>{point}</div>
         ))}
